f23e0363921eee5399ff713476e48356732ec24d
Piotr Sołtysek <p.soltysek@gmail.com>
2020-06-21 09:27:05 +0200

    first commit

---
+++ src/main/java/pl/camp/it/App.java
@@ -0,0 +1,16 @@
+package pl.camp.it;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) {
+        List<Integer> lista = new ArrayList<>();
+
+        lista.add(5);
+        lista.add(3);
+        lista.add(10);
+        lista.add(7);
+        lista.add(0);
+    }
+}
